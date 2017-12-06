614f2155170d8dd9be28846ce6307ac6c3333458
User <User@K3-512-06>
2017-12-06 20:32:27 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10, b = 11;
+
+            SwapInt(ref a, ref b);
+
+            Console.WriteLine("a = {0}, b = {1}", a, b);
+        }
+    }
+}
